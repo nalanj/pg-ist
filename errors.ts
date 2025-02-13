d3982f91f635a86e4bdc006e2c25b1ab3fa12812
Alan Johnson <nalanj@fastmail.com>
2025-02-12 22:56:28 -0500

    Actually finish ts port

--- errors.ts
+++ errors.ts
@@ -1,8 +1,15 @@
+import type pg from "pg";
+
 /**
  * ExactlyOneErrors are thrown when queryExactlyOne is called with a query that
  * returns no rows.
  */
-export class ExactlyOneError extends Error {}
+export class ExactlyOneError extends Error {
+	constructor(m: string) {
+		super(m);
+		Object.setPrototypeOf(this, ExactlyOneError.prototype);
+	}
+}
 
 /**
  * UniqueConstraintErrors are thrown when a unique constraint fails
@@ -10,12 +17,14 @@ export class ExactlyOneError extends Error {}
 export class UniqueConstraintError extends Error {
 	constraint;
 
-	constructor(message, constraint) {
+	constructor(message: string, constraint: string) {
 		super(message);
 		this.constraint = constraint;
+
+		Object.setPrototypeOf(this, UniqueConstraintError.prototype);
 	}
 
-	static fromDBError(error) {
+	static fromDBError(error: pg.DatabaseError) {
 		if (!error.constraint) {
 			throw new Error("Created constraint error without constraint property");
 		}
