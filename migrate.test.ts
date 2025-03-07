32a413693b2a5889097a05b9114683ed43ad9c48
Alan Johnson <nalanj@fastmail.com>
2025-03-06 21:54:13 -0500

    Add functions for inserting and getting latest migration

--- migrate.test.ts
+++ migrate.test.ts
@@ -1,15 +1,21 @@
 import assert from "node:assert";
-import { describe, it } from "node:test";
-import { createMigrationsTable } from "./migrate.js";
+import { beforeEach, describe, it } from "node:test";
+import {
+  type MigrationRow,
+  createMigrationsTable,
+  latestMigration,
+} from "./migrate.js";
 import { testDB } from "./test-help";
 
 const db = testDB();
 
 describe("migrations", () => {
+  beforeEach(async () => {
+    await db.query`DROP TABLE IF EXISTS migrations`;
+  });
+
   describe("createMigrationsTable", () => {
     it("creates a migrations table", async () => {
-      await db.query`DROP TABLE IF EXISTS migrations`;
-
       await createMigrationsTable(db);
 
       const result = await db.one<{
@@ -17,6 +23,31 @@ describe("migrations", () => {
       }>`SELECT count(id)::int FROM migrations`;
       assert.ok(result !== undefined);
       assert.equal(result.count, 0);
+    });
+  });
+
+  describe("insertMigration", () => {
+    it("inserts a migration", async () => {
+      await createMigrationsTable(db);
+
+      await db.query`INSERT INTO migrations (id) VALUES ('a')`;
+
+      const result =
+        await db.one<MigrationRow>`SELECT * FROM migrations WHERE id = 'a'`;
+      assert.ok(result !== undefined);
+      assert.equal(result.id, "a");
+    });
+  });
+
+  describe("latestMigration", () => {
+    it("gets latest MigrationRow", async () => {
+      await createMigrationsTable(db);
+
+      await db.query`INSERT INTO migrations (id) VALUES ('a'), ('b'), ('c')`;
+
+      const result = await latestMigration(db);
+      assert.ok(result !== undefined);
+      assert.equal(result.id, "c");
     });
   });
 });
--- migrate.ts
+++ migrate.ts
@@ -1,10 +1,25 @@
 import type { Queryable } from "./db.js";
 
+export type MigrationRow = {
+  id: string;
+  createdAt: Date;
+};
+
 export async function createMigrationsTable(db: Queryable) {
   await db.query`
     CREATE TABLE IF NOT EXISTS migrations (
       id TEXT NOT NULL PRIMARY KEY,
       created_at TIMESTAMPTZ DEFAULT NOW()
     )
+  `;
+}
+
+export async function insertMigration(db: Queryable, id: string) {
+  await db.query`INSERT INTO migrations (id) VALUES (${id})`;
+}
+
+export async function latestMigration(db: Queryable) {
+  return await db.one<MigrationRow>`
+    SELECT * FROM migrations ORDER BY id DESC LIMIT 1
   `;
 }
