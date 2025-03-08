31d5fb8387e8d1a8a4a38f80e104cbfce91f25d6
Alan Johnson <nalanj@fastmail.com>
2025-03-07 21:35:50 -0500

    Build out function for reading available migrations

--- migrate.test.ts
+++ migrate.test.ts
@@ -1,7 +1,9 @@
 import assert from "node:assert";
+import path from "node:path";
 import { beforeEach, describe, it } from "node:test";
 import {
   type MigrationRow,
+  availableMigrations,
   createMigrationsTable,
   latestMigration,
 } from "./migrate.js";
@@ -48,6 +50,39 @@ describe("migrations", () => {
       const result = await latestMigration(db);
       assert.ok(result !== undefined);
       assert.equal(result.id, "c");
+    });
+  });
+
+  describe("availableMigrations", () => {
+    it("gets available migrations", async () => {
+      const migrations = await availableMigrations(
+        "./fixtures/migrations/basic/",
+      );
+
+      assert.equal(migrations.length, 3);
+      assert.deepEqual(migrations, [
+        {
+          path: path.join(
+            process.cwd(),
+            "fixtures/migrations/basic/20241231011345-create-orgs.js",
+          ),
+          id: "20241231011345",
+        },
+        {
+          path: path.join(
+            process.cwd(),
+            "fixtures/migrations/basic/20250302090823-create-docs.js",
+          ),
+          id: "20250302090823",
+        },
+        {
+          path: path.join(
+            process.cwd(),
+            "fixtures/migrations/basic/20250307212006-create-users.js",
+          ),
+          id: "20250307212006",
+        },
+      ]);
     });
   });
 });
--- migrate.ts
+++ migrate.ts
@@ -1,8 +1,15 @@
+import fs from "node:fs/promises";
+import path from "node:path";
 import type { Queryable } from "./db.js";
 
 export type MigrationRow = {
   id: string;
   createdAt: Date;
+};
+
+export type MigrationPath = {
+  path: string;
+  id: string;
 };
 
 export async function createMigrationsTable(db: Queryable) {
@@ -22,4 +29,24 @@ export async function latestMigration(db: Queryable) {
   return await db.one<MigrationRow>`
     SELECT * FROM migrations ORDER BY id DESC LIMIT 1
   `;
+}
+
+const migrationRegex = /^(\d{14})-(.*)\.js$/;
+export async function availableMigrations(
+  migrationPath: string,
+): Promise<MigrationPath[]> {
+  const fileNames = await fs.readdir(migrationPath);
+
+  return fileNames.flatMap<MigrationPath>((filename) => {
+    const match = filename.match(migrationRegex);
+
+    if (!match) {
+      return [];
+    }
+
+    return {
+      path: path.resolve(process.cwd(), path.join(migrationPath, filename)),
+      id: match[1] as string,
+    };
+  });
 }
